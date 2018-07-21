8db1143ffae45eb8041fe114d03b321da430a3d5
Riley Jhi <riley.jhi@gmail.com>
2018-07-21 16:59:28 -0500

    skeleton for some things done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var screenDisabled = false;
+
+function showShipments() {
+    alert('shipments bruh');
+}
+
+function showLocations() {
+    alert('locations bruh');
+}
+
+function showCars() {
+    alert('cars bruh');
+}
+
+function toggleDisable() {
+    var disable = document.getElementById('disableScreen');
+    if(screenDisabled) {
+        disable.
+    }
+}
