4b385deff962e2a6dae190daba7cc8f513c0820e
Riley Jhi <riley.jhi@gmail.com>
2018-07-21 19:12:56 -0500

    changed all inputs to new customizd version

--- js/main.js
+++ js/main.js
@@ -5,11 +5,25 @@ var locations;
 var carType;
 
 var shipmentOptions = ['TEST1', 'TEST2', 'TEST3', 'TEST4', 'TEST5'];
+var locationOptions = ['LOCATION1', 'LOCATION2', 'LOCATION3'];
+var carOptions = ['CAR1', 'CAR2'];
 
 $(document).ready(function() {
     shipmentOptions.forEach(function(shipment) {
         var el = document.createElement("option");
         el.value = shipment;
         $('#shipmentOptions').append(el);
+    })
+
+    locationOptions.forEach(function(location) {
+        var el = document.createElement("option");
+        el.value = location;
+        $('#locationOptions').append(el);
+    })
+
+    carOptions.forEach(function(car) {
+        var el = document.createElement("option");
+        el.value = car;
+        $('#carOptions').append(el);
     })
 });
